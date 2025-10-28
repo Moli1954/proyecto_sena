893cdc6e7d6b55b6c19c01c693671e3f2b957fdb
Moli1954 <hasleynicollbellodiaz@gmail.com>
2025-10-28 11:44:24 -0500

    Segundo Commit

--- Services/IUserinterface.cs
+++ Services/IUserinterface.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using pryecto_nicol.Models;
 
 namespace pryecto_nicol.Services
 {
     public interface IUserinterface
     {
-        
+        void crearUsuario(usuarioModel1 usuario);
     }
 }
--- Views/implementacion/Userinterface.cs
+++ Views/implementacion/Userinterface.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using pryecto_nicol.Data;
+using pryecto_nicol.Models;
+using pryecto_nicol.Services;
 
 namespace pryecto_nicol.Views.implementacion
 {
-    public class Userinterface
+    public class Userinterface : IUserinterface
     {
-        
+        public void crearUsuario(usuarioModel1 usuario)
+        {
+            if (usuario != null)
+            {
+                dBContex.Usuarios.Add(usuario);
+                dBContex.SaveChangesAsync();
+
+            }
+        }
+        private readonly DBContext dBContex;
+        public Userinterface(DBContext dBContex)
+        {
+            this.dBContex = dBContex;
+        }
     }
 }
