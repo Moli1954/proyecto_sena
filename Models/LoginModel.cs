5bd5ab0b0dd83cd10f9455386f63a978346be748
Moli1954 <hasleynicollbellodiaz@gmail.com>
2025-11-21 10:31:17 -0500

    Login

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace pryecto_nicol.Models
 {
     public class LoginModel
     {
-        
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        public required string Login_correo { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
+        public required string Login_contrasena { get; set; }
+
     }
 }
